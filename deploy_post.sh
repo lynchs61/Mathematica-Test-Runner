7022db20a72813e7ead235dc2fa0fbf0c3a43f9e
Sean Lynch <lynchseanm@gmail.com>
2018-06-04 22:46:42 -0400

    GIM-223 postversion finito

--- deploy_post.sh
+++ deploy_post.sh
@@ -7,9 +7,16 @@ GIT_BRANCH=$(git symbolic-ref --short -q HEAD)
 # Get all the commits into one, well described commit for pushing
 COMMITS=$(git log --oneline $(git describe --tags --abbrev=0 @^)..@ | sed -E 's/^[a-f0-9]+ (.*)$/* \1/g')
 echo "$COMMITS"
-#git checkout ${GIT_BRANCH} mathematica-test-runner README.md test doc
-#git add .
-#git commit -m "Pulled in changes for deploying to Github"
-#git merge --no-edit v${PACKAGE_VERSION}
-##git push origin github_deploy
-##git push github master
+git checkout ${GIT_BRANCH} mathematica-test-runner README.md test doc
+git add .
+git commit -m "$COMMITS"
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+git tag ${PACKAGE_VERSION}
+git push origin github_deploy --tags
+git push github master --tags
