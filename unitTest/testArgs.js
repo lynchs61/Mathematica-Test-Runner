7d9cc376716a355e1ab77dea96c551d13c6bcb14
Sean Lynch <lynchseanm@gmail.com>
2018-06-07 10:56:08 -0400

    GIM-224 Added timeouts and updated last test

--- unitTest/testArgs.js
+++ unitTest/testArgs.js
@@ -11,6 +11,7 @@ const fs = require('fs')
 const path = require('path')
 
 let version = require(path.join(__dirname, '../package.json')).version
+const timeout = 10000
 
 describe('Mathematica-Test-Runner', () => {
   it('should handle no args', done => {
@@ -19,7 +20,7 @@ describe('Mathematica-Test-Runner', () => {
       stdout.split('\n')[0].should.equal('Must specify a target file or directory')
       done()
     })
-  })
+  }).timeout(timeout)
 
   it('should handle bogus args', done => {
     exec('./mathematica-test-runner -f --foo -b --bar', (error, stdout, stderr) => {
@@ -27,7 +28,7 @@ describe('Mathematica-Test-Runner', () => {
       stdout.split('\n')[0].should.equal('Invalid option \'-f\'')
       done()
     })
-  })
+  }).timeout(timeout)
 
   it('should print out the help text from --help', done => {
     let helpPath = path.join(__dirname, './assets/help.txt')
@@ -37,7 +38,7 @@ describe('Mathematica-Test-Runner', () => {
       stdout.should.equal(helpText)
       done()
     })
-  })
+  }).timeout(timeout)
 
   it('should print out the help text from -h', done => {
     let helpPath = path.join(__dirname, './assets/help.txt')
@@ -47,7 +48,7 @@ describe('Mathematica-Test-Runner', () => {
       stdout.should.equal(helpText)
       done()
     })
-  })
+  }).timeout(timeout)
 
   it('should print out the version from --version', done => {
     exec('./mathematica-test-runner --version', (error, stdout, stderr) => {
@@ -55,7 +56,7 @@ describe('Mathematica-Test-Runner', () => {
       stdout.should.equal(`Version: ${version}\n`)
       done()
     })
-  })
+  }).timeout(timeout)
 
   it('should print out the version from -V', done => {
     exec('./mathematica-test-runner -V', (error, stdout, stderr) => {
@@ -63,7 +64,7 @@ describe('Mathematica-Test-Runner', () => {
       stdout.should.equal(`Version: ${version}\n`)
       done()
     })
-  })
+  }).timeout(timeout)
 
   it('should handle invalid reporter', done => {
     exec('./mathematica-test-runner -R bogus', (error, stdout, stderr) => {
@@ -71,28 +72,45 @@ describe('Mathematica-Test-Runner', () => {
       stdout.split('\n')[0].should.equal('\'bogus\' is not a valid reporter. Please choose from {spec, tap, json, mathematica, junit}')
       done()
     })
-  })
-
-  it('should handle output to a file', done => {
-    exec('./mathematica-test-runner -R junit -o ./junit_tmp/test.xml test', (error, stdout, stderr) => {
-      if (error) throw error
-      stdout.should.equal('')
-      fs.stat(path.join(__dirname, '../junit_tmp/test.xml'), (err, stat) => {
-        should.not.exist(err)
-        done()
-      })
-    })
-  })
-
-  // This needs work to make it ... work. Needs a regex match because the time changes but that's a complicate multiline
-  // regex
-  // it('should produce the correct Spec output', done => {
-  //   const specText = fs.readFileSync(path.join(__dirname, './assets/expectedSpec.txt'), 'utf8')
-  //   const specMatch = new RegExp(specText)
-  //   exec('./mathematica-test-runner test', (error, stdout, stderr) => {
+  }).timeout(timeout)
+  //
+  // it('should handle output to a file', done => {
+  //   exec('./mathematica-test-runner -R junit -o ./junit_tmp/test.xml test', (error, stdout, stderr) => {
   //     if (error) throw error
-  //     stdout.should.match(specMatch)
-  //     done()
+  //     stdout.should.equal('')
+  //     fs.stat(path.join(__dirname, '../junit_tmp/test.xml'), (err, stat) => {
+  //       should.not.exist(err)
+  //       done()
+  //     })
   //   })
-  // }).timeout(10000)
+  // })
+
+  it('should produce the correct Spec output', done => {
+    const g = '\\[1;32m'
+    const r = '\\[0;31m'
+    const gr = '\\[0;37m'
+    const d = '\\[1;0m'
+    /* eslint-disable no-useless-escape */
+    const specMatchString = `\\s+Test Report: test1\.mt\
+\\s+.${g}. .${gr}test1-1.${d}\
+\\s+.${g}. .${gr}test1-2.${d}\
+\\s+Test Report: test2\.mt\
+\\s+.${r}. None.${d}\
+\\s+.${g}. .${gr}None.${d}\
+\\s+.${g}3 passing.${d}.${gr} \\(\\d+ms\\)\
+\\s+.${r}1 failing.${d}\
+\\s+Test Report: test2\.mt\
+\\s+None\
+\\s+.${r}Verification Error: expected 2 to equal 3.${d}\
+\\s+.${g}\\+ expected .${r}- actual\
+\\s+- 3.${g}\
+\\s+- 2.${d}`
+
+    const specMatchRegex = new RegExp(specMatchString)
+    exec('./mathematica-test-runner test', (error, stdout, stderr) => {
+      if (error) throw error
+      stdout.should.match(specMatchRegex)
+      done()
+    })
+  }).timeout(timeout)
 })
