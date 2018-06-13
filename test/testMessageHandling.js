6f26e08ddfcc5a6612d23c6eade0f2ad19e6f134
Sean lynch <lynchseanm@gmail.com>
2018-06-13 15:53:29 -0400

    GIM-226 Added tests for the message handling.

--- test/testMessageHandling.js
+++ test/testMessageHandling.js
@@ -13,9 +13,90 @@ const path = require('path')
 const timeout = 60000
 
 describe('Mathematica-Test-Runner', () => {
-  describe('Message Handling', () => {
-    it('should fail for now', () => {
-      throw new Error('Failing for now')
-    })
+  describe('TAP Message Handling', () => {
+    it('TAP without message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -i -R tap mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[6].should.equal('# pass 2')
+        lines[7].should.equal('# fail 1')
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+
+    it('TAP with message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -R tap mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[2].should.match(/^# Unexpected Message.*/)
+        lines[7].should.equal('# pass 1')
+        lines[8].should.equal('# fail 2')
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+  })
+  describe('Spec Message Handling', () => {
+    it('Spec without message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -i -R spec mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[6].should.match(/.*\[1;32m2 passing.*/)
+        lines[7].should.match(/.*\[0;31m1 failing.*/)
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+
+    it('Spec with message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -R spec mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[10].should.match(/^.*Unexpected Message.*/)
+        lines[6].should.match(/.*\[1;32m1 passing.*/)
+        lines[7].should.match(/.*\[0;31m2 failing.*/)
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+  })
+  describe('JUnit Message Handling', () => {
+    it('JUnit without message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -i -R junit mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[4].should.equal('    failures=\'1\'>')
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+
+    it('JUnit with message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -R junit mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[15].should.match(/^.*Unexpected Message.*/)
+        lines[4].should.equal('    failures=\'2\'>')
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+  })
+  describe('JSON Message Handling', () => {
+    it('JSON without message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -i -R json mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[5].should.equal('\t\t"passes":2,')
+        lines[6].should.equal('\t\t"failures":1')
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
+
+    it('JSON with message checking should have the correct failures and success', done => {
+      exec('./mathematica-test-runner -R json mathematicaTests/testWithMessages/', (error, stdout, stderr) => {
+        if (error) throw error
+        const lines = stdout.split('\n')
+        lines[19].should.match(/^.*expectedMsg.*/)
+        lines[5].should.equal('\t\t"passes":1,')
+        lines[6].should.equal('\t\t"failures":2')
+        done()
+      })
+    }).timeout(10 * timeout) // 10 min
   })
 })
